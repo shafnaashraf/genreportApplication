b3ffc6813e2742d9a1392ce962a94b7377a699ab
Shafna <shafna@payok.com>
2025-04-17 09:44:27 +0400

    changed support details table look

--- src/app/header/job/addSupportDetails/details/details.component.ts
+++ src/app/header/job/addSupportDetails/details/details.component.ts
@@ -1,21 +1,64 @@
 import {Component, Input} from '@angular/core';
 import {FormsModule} from '@angular/forms';
+import {NgForOf} from '@angular/common';
+
+interface SupportDetail {
+  drawingNo: string;
+  desc: string;
+  qty: string;
+  itemNo: string;
+  itemDesc: string;
+  specGrade: string;
+}
 
 @Component({
   selector: 'details-supp',
   templateUrl: 'details.component.html',
   styleUrl: 'details.component.css',
   imports: [
-    FormsModule
+    FormsModule,
+    NgForOf
   ],
   standalone: true
 })
+
+
 export class DetailsComponent{
 
   @Input()
   jobDetails!: { jobNumber: string; reportNumber: string; client : string ; project : string;title : string };
 
-  onSubmit() {
-    return 1;
+  newSupport: SupportDetail = {
+    drawingNo: '',
+    desc: '',
+    qty: '',
+    itemNo: '',
+    itemDesc: '',
+    specGrade: ''
+  };
+
+  supportList: SupportDetail[] = [];
+
+  saveSupport() {
+    if(this.newSupport.drawingNo != ''){
+      this.supportList.push({ ...this.newSupport });
+      this.newSupport = {
+        drawingNo: '',
+        desc: '',
+        qty: '',
+        itemNo: '',
+        itemDesc: '',
+        specGrade: ''
+      };
+    }
   }
+
+  editSupport(item: any) {
+    this.newSupport = { ...item };
+  }
+
+  isFieldInvalid(value: string): boolean {
+    return value === '';
+  }
+
 }
