0545f417708867c51d208171bb72062686ef3180
Shafna <shafna@payok.com>
2025-04-18 15:25:53 +0400

    material page added

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,10 +1,12 @@
 import { Routes } from '@angular/router';
 import {JobComponent} from './header/job/job.component';
 import {ProductionComponent} from './header/production/production.component';
+import {MaterialComponent} from './header/material/material.component';
 
 export const routes: Routes = [
   { path: 'job', component: JobComponent },
   { path: 'production', component: ProductionComponent},
+  { path: 'material', component: MaterialComponent},
   { path: '', redirectTo: 'about', pathMatch: 'full' }, // default route
   { path: '**', redirectTo: 'about' } // fallback route
 ];
